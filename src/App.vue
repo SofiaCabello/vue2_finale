be0eb0cbe4f4638637d1e3579e1fe076886b61a5
Kagurazaka Chisa <sofiacabello666@outlook.com>
2024-01-14 23:57:52 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <h1>今日头条</h1>
+    <!--路由出口-->
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
+// 创建Vue根实例，把router、store配置到根实例中
+// 把App根组件渲染到#app节点
 new Vue({
   router,
   store,
